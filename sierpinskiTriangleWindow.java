1094227fc0be402ff7b4119938b355adf6cebf2f
Colin Quick <quickcj@s.dscdk12.org>
2013-02-07 08:58:44 -0700

    started sierpinskiTriangle

---
+++ sierpinskiTriangleWindow.java
@@ -0,0 +1,63 @@
+public class sierpinskiTriangleWindow extends JFrame implements MouseListener {
+	
+
+
+
+/**
+ *  Constructor
+ *
+ * @param w - widlth of the window
+ * @param h - height of the window
+ */
+public sierpinskiTriangleWindow(int w, int h) {
+	super("sierpinskiTriangleWindow");
+		this.setBounds(200, 200, width, height);
+		this.setBackground(Color.black);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addMouseListener(this);
+		this.setVisible(true);
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
